302c56d0a1aaf5f970fe81b8e494858c34d1d96d
Kekichka <144151573+Kekichka@users.noreply.github.com>
2024-11-12 13:35:44 +0200

    Update lab4.cpp

--- lab4.cpp
+++ lab4.cpp
@@ -1,31 +1,41 @@
 #include <iostream>
-#include <vector>
+#include <string>
 using namespace std;
 
 struct Employee {
-    string lastName;
     string firstName;
+    string lastName;
     int age;
 };
 
-int main() {
-    int n;
-    cout << "Employee count: ";
-    cin >> n; 
+void inputEmployeeData(Employee& employee) {
+    cout << "Enter first name: ";
+    cin >> employee.firstName;
+    cout << "Enter last name: ";
+    cin >> employee.lastName;
+    cout << "Enter age: ";
+    cin >> employee.age;
+}
 
-    vector<Employee> employees(n);
-
-    for (int i = 0; i < n; i++) {
-        cout << "First name last name and age " << i + 1 << ": ";
-        cin >> employees[i].lastName >> employees[i].firstName >> employees[i].age;
-    }
-
-    cout << "empolyee age isnt > 28 :\n";
-    for (const auto& emp : employees) {
-        if (emp.age > 28) {
-            cout << emp.firstName << endl; 
+void printEmployeesOver28(const Employee employees[], int numberOfEmployees) {
+    cout << "Employees older than 28 years:\n";
+    for (int i = 0; i < numberOfEmployees; ++i) {
+        if (employees[i].age > 28) {
+            cout << employees[i].firstName << " " << employees[i].lastName << "\n";
         }
     }
+}
+
+int main() {
+    const int employeeNumber = 5;
+    Employee employees[employeeNumber];
+
+    for (int i = 0; i < employeeNumber; ++i) {
+        cout << "Enter data for employee " << i + 1 << ":\n";
+        inputEmployeeData(employees[i]);
+    }
+
+    printEmployeesOver28(employees, employeeNumber);
 
     return 0;
 }
